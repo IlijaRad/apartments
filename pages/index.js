01265c96179f073f5b74099663573fbfbd3d6c4f
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-10-20 22:07:17 +0200

    - bugfix homepage images not centered on small screens

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
       <button className="mx-auto mt-10 block rounded-lg border border-gray-200 py-2.5 px-7 text-lg font-semibold tracking-tight text-gray-700">
         Make a reservation now
       </button>
-      <div className="-mx-8 mt-[140px] flex gap-x-[45px] gap-y-0">
+      <div className="-mx-8 mt-[140px] flex justify-center gap-x-[45px]">
         {IMAGES.map(({ id, src, alt, position }) => (
           <div
             key={id}
