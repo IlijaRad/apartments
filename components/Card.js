6bc7443961d2211a17c911c11fdae7ca5c4c7674
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-10-20 23:08:23 +0200

    - about section

---
+++ components/Card.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+const Card = ({
+  title,
+  description,
+  guests,
+  view,
+  size,
+  hightlights,
+  imgSrc,
+}) => {
+  return (
+    <div className="rounded-2xl border border-gray-200">
+      <Image className="rounded-t-2xl" width={1024} src={imgSrc} />
+      <div className="px-9 py-7">
+        <h3 className="text-2xl font-semibold text-gray-900">{title}</h3>
+        <div className="mt-8 text-sm font-semibold leading-5 text-gray-700">
+          Short description
+        </div>
+        <p className="mt-3.5 leading-7 text-gray-600">{description}</p>
+        <div className="mt-7 text-sm font-semibold text-gray-700">
+          Specifications
+        </div>
+        <div className="mt-6">
+          <div className="grid grid-cols-[70px_auto] text-gray-600">
+            <div className="text-xs font-bold uppercase leading-5">Guests</div>
+            <div className="text-sm font-medium">{guests}</div>
+          </div>
+          <div className="mt-3.5 grid grid-cols-[70px_auto] text-gray-600">
+            <div className="text-xs font-bold uppercase leading-5">View</div>
+            <div className="text-sm font-medium">{view}</div>
+          </div>
+          <div className="mt-3.5 grid grid-cols-[70px_auto] text-gray-600">
+            <div className="text-xs font-bold uppercase leading-5">Size</div>
+            <div className="text-sm font-medium">
+              (US) - {size.us} ft<sup>2</sup> (EU) - {size.eu} m<sup>2</sup>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 text-sm font-semibold text-gray-700">
+          Hightlights
+        </div>
+        <div className="mt-3 flex gap-x-4">
+          {hightlights.map((hightlight, ix) => (
+            <div
+              key={ix}
+              className="rounded-md border border-gray-200 py-1.5 px-4 text-sm font-medium text-gray-700"
+            >
+              <span># </span>
+              {hightlight}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ components/ContactSection.js
@@ -0,0 +1,14 @@
+const ContactSection = () => {
+  return (
+    <div id="contact" className="pt-[166px]">
+      <h2 className="text-center font-semibold tracking-wide text-primary-500">
+        Contact us
+      </h2>
+      <h3 className="mt-3.5 text-center text-5xl font-semibold leading-[52px] tracking-tight text-gray-900">
+        Get in contact with us
+      </h3>
+    </div>
+  );
+};
+
+export default ContactSection;
