00038a0d932d22c1eecfc846a266ec1e40beb10e
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-10-23 14:59:59 +0200

    - remove border animation from header

--- components/Header.js
+++ components/Header.js
@@ -11,44 +11,46 @@ const Header = () => {
     <div
       className={
         !isOpenMenu
-          ? "max-h-20 overflow-hidden border-b border-gray-100 transition-all"
+          ? "max-h-20 overflow-hidden transition-all"
           : "max-h-[282px] transition-all"
       }
     >
-      <header className="mx-auto flex max-w-7xl items-center justify-center px-6 py-4 md:pt-7 md:pb-[27px]">
-        <div className="flex w-full items-center justify-between gap-x-6">
-          <div className="text-sm font-semibold uppercase text-slate-900 md:text-base md:tracking-wide lg:text-xl lg:tracking-wider">
-            <a href={HOME} className="block h-6 w-[300px]">
-              <Logo />
-            </a>
+      <div className="h-20 border-b border-gray-100">
+        <header className="mx-auto flex max-w-7xl items-center justify-center px-6 py-4 md:pt-7 md:pb-[27px]">
+          <div className="flex w-full items-center justify-between gap-x-6">
+            <div className="text-sm font-semibold uppercase text-slate-900 md:text-base md:tracking-wide lg:text-xl lg:tracking-wider">
+              <a href={HOME} className="block h-6 w-[300px]">
+                <Logo />
+              </a>
+            </div>
+            <nav className="hidden md:block">
+              <ul className="flex gap-x-4 lg:gap-x-12">
+                {LINKS.map(({ id, label, href }) => (
+                  <li
+                    key={id}
+                    className="transition-color  text-gray-700 hover:text-gray-800 lg:text-base lg:font-medium"
+                  >
+                    <a href={href} className="block">
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <button
+              onClick={() => setIsOpenMenu((isOpenMenu) => !isOpenMenu)}
+              className="rounded-full p-2.5 text-gray-600 transition-all focus:text-primary-400 focus:outline-none md:hidden"
+            >
+              <Bars3Icon
+                className={isOpenMenu ? "hidden" : "h-7 w-7 text-gray-600"}
+              />
+              <XMarkIcon
+                className={isOpenMenu ? "h-7 w-7 text-gray-600" : "hidden"}
+              />
+            </button>
           </div>
-          <nav className="hidden md:block">
-            <ul className="flex gap-x-4 lg:gap-x-12">
-              {LINKS.map(({ id, label, href }) => (
-                <li
-                  key={id}
-                  className="transition-color  text-gray-700 hover:text-gray-800 lg:text-base lg:font-medium"
-                >
-                  <a href={href} className="block">
-                    {label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <button
-            onClick={() => setIsOpenMenu((isOpenMenu) => !isOpenMenu)}
-            className="rounded-full p-2.5 text-gray-600 transition-all focus:text-primary-400 focus:outline-none md:hidden"
-          >
-            <Bars3Icon
-              className={isOpenMenu ? "hidden" : "h-7 w-7 text-gray-600"}
-            />
-            <XMarkIcon
-              className={isOpenMenu ? "h-7 w-7 text-gray-600" : "hidden"}
-            />
-          </button>
-        </div>
-      </header>
+        </header>
+      </div>
       <div
         className={
           isOpenMenu
@@ -61,7 +63,7 @@ const Header = () => {
             key={id}
             href={href}
             onClick={() => setIsOpenMenu((isOpenMenu) => !isOpenMenu)}
-            className="transition-color block cursor-pointer border-b border-gray-200 px-6 py-4 text-sm font-medium text-slate-900 first:border-t"
+            className="transition-color block cursor-pointer border-b border-gray-100 px-6 py-4 text-sm font-medium text-slate-900"
           >
             {label}
           </a>
